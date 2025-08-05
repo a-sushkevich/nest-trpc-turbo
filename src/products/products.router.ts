7d82556126ac3891c0a6f493a304f4e8b41b869c
aliaksandr sushkevich <117445970+a-sushkevich@users.noreply.github.com>
2025-08-05 23:41:34 +0300

    rest of product crud

--- src/products/products.router.ts
+++ src/products/products.router.ts
@@ -1,10 +1,35 @@
-import { Input, Mutation, Router } from 'nestjs-trpc';
+import { Input, Mutation, Query, Router } from 'nestjs-trpc';
 import { ProductService } from './products.service';
 import { Product, productsSchema } from './products.schema';
+import z from 'zod';
 
 @Router({ alias: 'products' })
 export class ProductsRouter {
   constructor(private readonly productsService: ProductService) {}
+
+  @Query({ input: z.object({ id: z.string() }), output: productsSchema })
+  getProductById(@Input('id') id: string) {
+    return this.productsService.getProductById(id);
+  }
+
+  @Query({ output: z.array(productsSchema) })
+  getAllProducts(@Input('id') id: string) {
+    return this.productsService.getAllProducts();
+  }
+
+  @Mutation({
+    input: z.object({
+      id: z.string(),
+      data: productsSchema.partial(),
+    }),
+    output: productsSchema,
+  })
+  updateProduct(
+    @Input('id') id: string,
+    @Input('data') data: Partial<Product>,
+  ) {
+    return this.productsService.updateProduct(id, data);
+  }
 
   @Mutation({
     input: productsSchema,
@@ -12,5 +37,13 @@ export class ProductsRouter {
   })
   createProduct(@Input() productsData: Product) {
     return this.productsService.createProduct(productsData);
+  }
+
+  @Mutation({
+    input: z.object({ id: z.string() }),
+    output: z.boolean(),
+  })
+  deleteProduct(@Input('id') id: string) {
+    return this.productsService.deleteProduct(id);
   }
 }
--- src/products/products.service.ts
+++ src/products/products.service.ts
@@ -1,5 +1,6 @@
 import { Injectable } from '@nestjs/common';
 import { Product } from './products.schema';
+import { TRPCError } from '@trpc/server';
 
 @Injectable()
 export class ProductService {
@@ -8,5 +9,50 @@ export class ProductService {
   createProduct(productData: Product) {
     this.products.push(productData);
     return productData;
+  }
+
+  getProductById(id: string) {
+    const product = this.products.find((product) => product.id === id);
+
+    if (!product) {
+      throw new TRPCError({
+        message: 'Product not found',
+        code: 'NOT_FOUND',
+      });
+    }
+
+    return product;
+  }
+
+  getAllProducts() {
+    return this.products;
+  }
+
+  updateProduct(id: string, data: Partial<Product>) {
+    const productIndex = this.products.findIndex(
+      (product) => product.id === id,
+    );
+    if (productIndex === -1) {
+      throw new TRPCError({
+        message: 'Product not found',
+        code: 'NOT_FOUND',
+      });
+    }
+    this.products[productIndex] = {
+      ...this.products[productIndex],
+      ...data,
+    };
+    return this.products[productIndex];
+  }
+
+  deleteProduct(id: string) {
+    const productIndex = this.products.findIndex(
+      (product) => product.id === id,
+    );
+    if (productIndex === -1) {
+      return false;
+    }
+    this.products.splice(productIndex, 1);
+    return true;
   }
 }
